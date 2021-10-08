f87334c36e63b074995774f242cd8b9d26b98fb4
Force_human <66125890+Ffferrar@users.noreply.github.com>
2021-10-08 03:30:22 +0300

    08.10.2021 Add Resize

--- StackPush.cpp
+++ StackPush.cpp
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include "structures.h"
+
+#define RISE_V_STACK 2
+
+void StackResize(Stack *sstack)
+{
+    sstack->capacity *= RISE_V_STACK;
+    sstack->data = (int*) calloc (sstack->capacity, sizeof(int));
+}
 
 void StackPush(Stack *sstack, int value)
 {
     assert(sstack);
+    if ( sstack->ssize == sstack->capacity - 1 )
+    {
+       StackResize(sstack);
+    }
     sstack->data[sstack -> ssize] = value;
     sstack->ssize++;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     int k = 10;
     Stack S;
     StackCtr(&S, 10);
-    for (int i=0;i<10;i++)
+    for (int i=0; i<150; i++)
     {
         StackPush(&S, i);
     }
