7b98ab327efb7f23fede07c82c556257c60c8afc
Force_human <66125890+Ffferrar@users.noreply.github.com>
2021-10-09 03:02:00 +0300

    09.10.21 Add CPU interpretator. Working vers

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int k = 10;
+    /*int k = 10;
     Stack S;
     StackCtr(&S, 10);
     for (int i=0; i<150; i++)
@@ -11,6 +11,27 @@ int main()
         StackPush(&S, i);
     }
 
-    printf("%d", StackPop(&S) );
+    printf("%d", StackPop(&S) ); */
+    int Start_Var = 0;
+    printf("\nKozhanov Gleb, B05-120\n");
+    printf("\nThe Stack & CPU project\n\n");
+
+    printf("If You want to work with CPU, ENTER 1\nElse ENTER 0: ");
+    scanf("%d", &Start_Var);
+    char trash;
+    scanf("%c", &trash);
+    switch (Start_Var)
+    {
+        case 0: break;
+
+        case 1: Stack S;
+                StackCtr(&S, 10); //вызываю функцию для реализации всякого
+
+                User_Work(&S);
+                break;
+
+        default: printf("main(): ERROR: Start_Var = %d/n", Start_Var);
+                return 1;
+    }
     return 0;
 }
--- structures.h
+++ structures.h
@@ -13,3 +13,11 @@ void StackPush(Stack *sstack, int value);
 int StackPop(Stack *sstack);
 
 void StackDctor();
+
+int command_compare(char *data1, int length1, char *data2, int length2);
+
+int inf_proc (Stack *S, char user_ch);
+
+void CPU (int COMMAND_NUM, int user_number, Stack *S);
+
+void User_Work(Stack *S);
